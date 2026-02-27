ba4b60be6a546416758cfa993d956d60f37ca600
Shree Kumar Chandan Charchit <shreekumarchandancharchit@gmail.com>
2026-02-27 19:46:57 +0530

    Add Makefile, ruff configuration, and update main application structure

---
+++ Makefile
@@ -0,0 +1,5 @@
+format:
+	ruff check --fix src
+
+run:
+	python src/learn_backend/main.py
--- src/learn_backend/main.py
+++ src/learn_backend/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from learn-backend!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
